d9df13e0108de7abb3fb31f24535731e99a4a2d9
blinasmailaj <74669741+blinasmailaj@users.noreply.github.com>
2021-05-16 23:08:13 +0200

    Polybius Square Cipher Desktop

---
+++ polybiusSquareCipherDesktop/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace polybiusSquareCipherDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.keyBox = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.encBox = new System.Windows.Forms.TextBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.dctBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Teksti ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(33, 65);
+            this.txtBox.Margin = new System.Windows.Forms.Padding(2);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(179, 20);
+            this.txtBox.TabIndex = 1;
+            this.txtBox.Validating += new System.ComponentModel.CancelEventHandler(this.txtBox_Validating);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(30, 87);
+            this.label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(113, 15);
+            this.label.TabIndex = 2;
+            this.label.Text = "Keyword (Optional)";
+            // 
+            // keyBox
+            // 
+            this.keyBox.Location = new System.Drawing.Point(32, 106);
+            this.keyBox.Margin = new System.Windows.Forms.Padding(2);
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(179, 20);
+            this.keyBox.TabIndex = 3;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnEncrypt.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncrypt.Location = new System.Drawing.Point(32, 208);
+            this.btnEncrypt.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(84, 37);
+            this.btnEncrypt.TabIndex = 4;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = false;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 128);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Teksti i Enkriptuar";
+            // 
+            // encBox
+            // 
+            this.encBox.Location = new System.Drawing.Point(32, 145);
+            this.encBox.Margin = new System.Windows.Forms.Padding(2);
+            this.encBox.Name = "encBox";
+            this.encBox.Size = new System.Drawing.Size(179, 20);
+            this.encBox.TabIndex = 6;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecrypt.Location = new System.Drawing.Point(120, 208);
+            this.btnDecrypt.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(91, 37);
+            this.btnDecrypt.TabIndex = 7;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // dctBox
+            // 
+            this.dctBox.Location = new System.Drawing.Point(32, 184);
+            this.dctBox.Margin = new System.Windows.Forms.Padding(2);
+            this.dctBox.Name = "dctBox";
+            this.dctBox.Size = new System.Drawing.Size(179, 20);
+            this.dctBox.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 167);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Teksti i Dekriptuar";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(510, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 37);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(8, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 21);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Polybius Square Cipher";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(612, 281);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dctBox);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.encBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.keyBox);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Plaintext";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox keyBox;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox encBox;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.TextBox dctBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ polybiusSquareCipherDesktop/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace polybiusSquareCipherDesktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private char[,] square(string word)
+        {
+            char[,] grid = new char[5, 5];
+
+            StringBuilder alphabet = new StringBuilder();
+            for (int i = 'A'; i < 'A' + 26; i++)
+                if ((char)i != 'J')
+                    alphabet.Append((char)i);
+
+            word = word.ToUpper();
+
+            for (int i = 0; i < word.Length; i++)
+                alphabet.Replace(word[i].ToString(), "");
+
+            int a = 0;
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                {
+                    if (a < word.Length)
+                    {
+                        for (int l = 0; l < a; l++)
+                        {
+                            if (a < word.Length && word[l] == word[a])
+                            {
+                                a++;
+                                l = -1;
+                            }
+                        }
+                        if (a < word.Length)
+                        {
+                            grid[i, j] = word[a];
+                            a++;
+                        }
+                    }
+                    else
+                    {
+                        grid[i, j] = alphabet[0];
+                        alphabet.Remove(0, 1);
+                    }
+                }
+            return grid;
+        }
+
+        private string encryptPolybius(string plaintext, char[,] grid)
+        {
+            StringBuilder index = new StringBuilder("");
+            plaintext = plaintext.Replace("I", "J");
+            plaintext = plaintext.Replace(" ", "");
+
+
+
+
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                char chA = plaintext[i];
+                int row = 0, col = 0;
+                for (int j = 0; j < 5; j++)
+                    for (int a = 0; a < 5; a++)
+                    {
+                        if (grid[j, a] == chA)
+                        {
+                            row = j + 1;
+                            col = a + 1;
+                        }
+                    }
+                index.Append(row.ToString() + col.ToString());
+            }
+
+            return index.ToString();
+        }
+        private string decryptPolybius(string ciphertext, char[,] grid)
+        {
+            StringBuilder letters = new StringBuilder();
+            for (int i = 0; i < ciphertext.Length; i += 2)
+            {
+                char enc11 = ciphertext[i];
+                char enc21 = ciphertext[i + 1];
+                int enc1 = int.Parse(enc11.ToString()) - 1;
+                int enc2 = int.Parse(enc21.ToString()) - 1;
+                char dec = 'j';
+                for (int j = 0; j < 5; j++)
+                    for (int a = 0; a < 5; a++)
+                    {
+                        if (j == enc1 && a == enc2)
+                        {
+                            dec = grid[j, a];
+                        }
+                    }
+                letters.Append(dec);
+            }
+
+            return letters.ToString().ToLower();
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (ValidateChildren(ValidationConstraints.Enabled))
+            {
+                char[,] grid = square(keyBox.Text);
+                string encryptedText = encryptPolybius(txtBox.Text.ToUpper(), grid);
+                encBox.Text = encryptedText;
+            }
+
+
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            char[,] grid = square(keyBox.Text);
+            string decryptedText = decryptPolybius(encBox.Text, grid);
+            dctBox.Text = decryptedText;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            txtBox.Clear();
+            keyBox.Clear();
+            encBox.Clear();
+            dctBox.Clear();
+        }
+
+        private void txtBox_Validating(object sender, CancelEventArgs e)
+        {
+            string word = txtBox.Text;
+            if (string.IsNullOrEmpty(word))
+            {
+                e.Cancel = true;
+                txtBox.Focus();
+                errorProvider1.SetError(txtBox, "Please type something");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(txtBox, null);
+            }
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (!char.IsLetterOrDigit(word[i]))
+                {
+                    e.Cancel = true;
+                    txtBox.Focus();
+                    errorProvider1.SetError(txtBox, "Input must be a digit or a number");
+                }
+                else
+                {
+                    e.Cancel = false;
+                    errorProvider1.SetError(txtBox, null);
+                }
+
+            }
+
+        }
+    }
+}
